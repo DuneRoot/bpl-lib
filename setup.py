a3e341023f7226918146ff2c29106e8276aa837a
johnyob <johnyob132@gmail.com>
2018-07-25 16:35:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
-import sys
-
 import setuptools
 
 requires = [
-    "base58",
+    "base58>=1.0.0",
     "ecdsa"
 ]
 
-tests_require = []
-
-extras_require = {
-    'test': tests_require,
-    'dev': requires + tests_require
-}
-
-setup_requires = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
+packages = [
+    "bpl_lib.address",
+    "bpl_lib.crypto",
+    "bpl_lib.helpers",
+    "bpl_lib.network",
+    "bpl_lib.time",
+    "bpl_lib.transactions"
+]
 
 setuptools.setup(
     name="bpl-lib",
     description="A library for the Blockpool Blockchain.",
     version="0.0.1",
+    url="https://github.com/DuneRoot/bpl-lib",
     author="Alistair O'Brien",
     author_email="alistair.o'brien@ellesmere.com",
-    packes=setuptools.find_packages(exclude=["tests", "tests.*"]),
-    install_requires=requires,
-    extras_require=extras_require,
-    tests_require=tests_require,
-    setup_requires=setup_requires
+    packages=packages,
+    install_requires=requires
 )
